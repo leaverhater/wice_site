a6643a47437b68c30245cf1aa8d3c9cb78eacf4d
vvoznyuk <vdvoznyuk@gmail.com>
2016-01-14 16:49:16 +0300

    Now using sqlite

--- wice_site/settings.py
+++ wice_site/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'wice_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wice_site',
-        'USER': 'postgres',
-        'PASSWORD': 'EnjoyBmstu14',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
