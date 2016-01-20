bdbefdedf7fdc8974b2bb55d75278bcc003d9a30
vvoznyuk <vdvoznyuk@gmail.com>
2016-01-20 07:32:01 +0300

    - enhanced project details page

--- wice_site/urls.py
+++ wice_site/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^', include('info.urls', namespace='info')),
+                  url(r'^admin/', include(admin.site.urls)),
+                  # url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+                  url(r'^', include('info.urls', namespace='info')),
 
-]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
